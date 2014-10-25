c6f0c2d95785aef37169970f82360c25234bb5ea
Tim Cooper <tim.cooper@layeh.com>
2014-10-25 16:51:39 -0300

    format client_incoming.go

--- client_incoming.go
+++ client_incoming.go
@@ -23,27 +23,27 @@ func clientIncoming(client *Client) {
 	conn := client.connection
 
 	for {
-    var pType uint16
-    var pLength uint32
+		var pType uint16
+		var pLength uint32
 
-    if err := binary.Read(conn, binary.BigEndian, &pType); err != nil {
-      return
-    }
-    if err := binary.Read(conn, binary.BigEndian, &pLength); err != nil {
-      return
-    }
-    pLengthInt := int(pLength)
-    if pLengthInt > maximumPacketLength {
-      return
-    }
-    data := make([]byte, pLengthInt)
-    if _, err := io.ReadFull(conn, data); err != nil {
-      return
-    }
-    if handle, ok := handlers[pType]; ok {
-      if err := handle(client, data); err != nil {
-        // TODO: log error?
-      }
-    }
+		if err := binary.Read(conn, binary.BigEndian, &pType); err != nil {
+			return
+		}
+		if err := binary.Read(conn, binary.BigEndian, &pLength); err != nil {
+			return
+		}
+		pLengthInt := int(pLength)
+		if pLengthInt > maximumPacketLength {
+			return
+		}
+		data := make([]byte, pLengthInt)
+		if _, err := io.ReadFull(conn, data); err != nil {
+			return
+		}
+		if handle, ok := handlers[pType]; ok {
+			if err := handle(client, data); err != nil {
+				// TODO: log error?
+			}
+		}
 	}
 }
