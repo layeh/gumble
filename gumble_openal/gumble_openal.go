17b98a8d2eaf27c74184b72194e887e0a084bfec
Tim Cooper <tim.cooper@layeh.com>
2014-11-19 19:15:57 -0400

    gumble_openal: add incoming audio support

--- gumble_openal/gumble_openal.go
+++ gumble_openal/gumble_openal.go
@@ -17,10 +17,17 @@ type Stream struct {
 	deviceSource *openal.CaptureDevice
 	outgoing     chan<- gumble.AudioPacket
 	sourceStop   chan bool
+
+	deviceSink  *openal.Device
+	contextSink *openal.Context
+	incoming    chan gumble.AudioPacket
+	userStreams map[uint]openal.Source
 }
 
 func New() (*Stream, error) {
-	stream := &Stream{}
+	stream := &Stream{
+		userStreams: make(map[uint]openal.Source),
+	}
 	return stream, nil
 }
 
@@ -34,10 +41,29 @@ func (s *Stream) OnAttachSource(outgoing chan<- gumble.AudioPacket) error {
 	return nil
 }
 
+func (s *Stream) OnAttachSink() (chan<- gumble.AudioPacket, error) {
+	s.deviceSink = openal.OpenDevice("")
+	s.contextSink = s.deviceSink.CreateContext()
+	s.contextSink.Activate()
+
+	s.incoming = make(chan gumble.AudioPacket)
+	go s.sinkRoutine()
+	return s.incoming, nil
+}
+
 func (s *Stream) OnDetach() {
-	if s.deviceSource != nil {
+	if s.outgoing != nil {
 		s.deviceSource.CaptureCloseDevice()
 		s.deviceSource = nil
+		s.outgoing = nil
+	}
+	if s.incoming != nil {
+		close(s.incoming)
+		s.incoming = nil
+		s.contextSink.Destroy()
+		s.deviceSink.CloseDevice()
+		s.contextSink = nil
+		s.deviceSink = nil
 	}
 }
 
@@ -47,7 +73,7 @@ func (s *Stream) StartSource() error {
 	}
 	s.deviceSource.CaptureStart()
 	s.sourceStop = make(chan bool)
-	go source_routine(s, s.sourceStop)
+	go s.sourceRoutine()
 	return nil
 }
 
@@ -61,9 +87,50 @@ func (s *Stream) StopSource() error {
 	return nil
 }
 
-func source_routine(s *Stream, stop chan bool) {
+func (s *Stream) sinkRoutine() {
+	incoming := s.incoming
+
+	byteBuffer := make([]byte, 9600) // Supports up to a 100ms buffer
+
+	for {
+		packet, ok := <-incoming
+		if !ok {
+			return
+		}
+		samples := len(packet.Pcm)
+		if samples*2 > cap(byteBuffer) {
+			continue
+		}
+		var source openal.Source
+		if userSource, ok := s.userStreams[packet.Sender.Session()]; !ok {
+			source = openal.NewSource()
+			s.userStreams[packet.Sender.Session()] = openal.NewSource()
+		} else {
+			source = userSource
+		}
+
+		for i, value := range packet.Pcm {
+			binary.LittleEndian.PutUint16(byteBuffer[i*2:], uint16(value))
+		}
+
+		var buffer openal.Buffer
+		for source.BuffersProcessed() > 0 {
+			openal.DeleteBuffer(source.UnqueueBuffer())
+		}
+		buffer = openal.NewBuffer()
+		buffer.SetData(openal.FormatMono16, byteBuffer[0:samples*2], gumble.SampleRate)
+		source.QueueBuffer(buffer)
+
+		if source.State() != openal.Playing {
+			source.Play()
+		}
+	}
+}
+
+func (s *Stream) sourceRoutine() {
 	packet := gumble.AudioPacket{}
 	outgoing := s.outgoing
+	stop := s.sourceStop
 	int16Buffer := make([]int16, 480)
 	ticker := time.NewTicker(10 * time.Millisecond)
 	defer ticker.Stop()
