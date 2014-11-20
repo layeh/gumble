042e7c7139349a7d78a23b71011af18b20c71267
Tim Cooper <tim.cooper@layeh.com>
2014-11-20 18:01:49 -0400

    change Users and Channels methods to not operate on pointers

--- gumble/channels.go
+++ gumble/channels.go
@@ -4,34 +4,34 @@ type Channels map[uint]*Channel
 
 // Create adds a new channel with the given id to the collection. If a channel
 // with the given id already exists, it is overwritten.
-func (c *Channels) Create(id uint) *Channel {
+func (c Channels) Create(id uint) *Channel {
 	channel := &Channel{
-		id: uint32(id),
+		id:       uint32(id),
 	}
-	(*c)[id] = channel
+	c[id] = channel
 	return channel
 }
 
 // ById returns a pointer to the channel with the given id, null if no channel
 // exists with the given id.
-func (c *Channels) ById(id uint) *Channel {
-	return (*c)[id]
+func (c Channels) ById(id uint) *Channel {
+	return c[id]
 }
 
 // Exists returns true if a channel with the given id exists in the collection.
-func (c *Channels) Exists(id uint) bool {
-	_, ok := (*c)[id]
+func (c Channels) Exists(id uint) bool {
+	_, ok := c[id]
 	return ok
 }
 
 // Delete removes the channel with the given id from the collection.
-func (c *Channels) Delete(id uint) {
-	delete(*c, id)
+func (c Channels) Delete(id uint) {
+	delete(c, id)
 }
 
 // TODO: document me
-func (c *Channels) Find(names ...string) *Channel {
-	root := (*c)[0]
+func (c Channels) Find(names ...string) *Channel {
+	root := c[0]
 	if names == nil || root == nil {
 		return root
 	}
--- gumble/users.go
+++ gumble/users.go
@@ -4,28 +4,28 @@ type Users map[uint]*User
 
 // Create adds a new user with the given session to the collection. If a user
 // with the given session already exists, it is overwritten.
-func (u *Users) Create(session uint) *User {
+func (u Users) Create(session uint) *User {
 	user := &User{
 		session: uint32(session),
 	}
-	(*u)[session] = user
+	u[session] = user
 	return user
 }
 
 // BySession returns a pointer to the user with the given session, null if no
 // user exists with the given session.
-func (u *Users) BySession(session uint) *User {
-	return (*u)[session]
+func (u Users) BySession(session uint) *User {
+	return u[session]
 }
 
 // Exists returns true if a user with the given session exists in the
 // collection.
-func (u *Users) Exists(session uint) bool {
-	_, ok := (*u)[session]
+func (u Users) Exists(session uint) bool {
+	_, ok := u[session]
 	return ok
 }
 
 // Delete removes the user with the given session from the collection.
-func (u *Users) Delete(session uint) {
-	delete(*u, session)
+func (u Users) Delete(session uint) {
+	delete(u, session)
 }
