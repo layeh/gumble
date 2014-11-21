d9cb350829ce64249d57c4ccaa069fd608e9de53
Tim Cooper <tim.cooper@layeh.com>
2014-11-20 22:21:35 -0400

    increase varint.ReadFrom range

--- gumble/varint/read.go
+++ gumble/varint/read.go
@@ -9,27 +9,32 @@ import (
 // On success, the function returns the varint as a int64, the number of bytes
 // consumed, and nil.
 func ReadFrom(r io.Reader) (int64, int64, error) {
-	var buf [3]byte
-
+	var buffer [3]byte
 	if b, err := ReadByte(r); err != nil {
 		return 0, 0, err
 	} else {
-		buf[0] = b
+		if (b & 0x80) == 0 {
+			return int64(b), 1, nil
+		}
+		if (b & 0xC0) == 0x80 {
+			if n, err := io.ReadFull(r, buffer[0:1]); err != nil {
+				return 0, int64(n + 1), err
+			}
+			return int64(b&0x3F)<<8 | int64(buffer[0]), 2, nil
+		}
+		if (b & 0xE0) == 0xC0 {
+			if n, err := io.ReadFull(r, buffer[0:2]); err != nil {
+				return 0, int64(n + 1), err
+			}
+			return int64(b&0x1F)<<8 | int64(buffer[0])<<8 | int64(buffer[1]), 3, nil
+		}
+		if (b & 0xF0) == 0xE0 {
+			if n, err := io.ReadFull(r, buffer[0:3]); err != nil {
+				return 0, int64(n + 1), err
+			}
+			return int64(b&0xF)<<8 | int64(buffer[0])<<8 | int64(buffer[1])<<8 | int64(buffer[2]), 4, nil
+		}
 	}
-	if (buf[0] & 0x80) == 0 {
-		return int64(buf[0] & 0x7F), 1, nil
-	}
-
-	if b, err := ReadByte(r); err != nil {
-		return 0, 1, err
-	} else {
-		buf[1] = b
-	}
-
-	if (buf[0] & 0xC0) == 0x80 {
-		return (int64(buf[0]&0x3F) << 8) | int64(buf[1]), 2, nil
-	}
-
 	return 0, 0, ErrOutOfRange
 }
 
