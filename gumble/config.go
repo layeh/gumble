1d26d2fe88bb0f66192b01bc70dd26279da599e5
Tim Cooper <tim.cooper@layeh.com>
2014-11-27 16:29:56 -0400

    change Config.writeTo to AccessTokens.writeTo

--- gumble/config.go
+++ gumble/config.go
@@ -9,18 +9,20 @@ import (
 )
 
 type Config struct {
-	Username string   // Client username
-	Password string   // Client password (usually not required)
-	Address  string   // Server address, including port (e.g. localhost:64738)
-	Tokens   []string // Server access tokens
+	Username string       // Client username
+	Password string       // Client password (usually not required)
+	Address  string       // Server address, including port (e.g. localhost:64738)
+	Tokens   AccessTokens // Server access tokens
 
 	TlsConfig tls.Config
 	Dialer    net.Dialer
 }
 
-func (c *Config) writeTo(w io.Writer) (int64, error) {
+type AccessTokens []string
+
+func (at AccessTokens) writeTo(w io.Writer) (int64, error) {
 	packet := MumbleProto.Authenticate{
-		Tokens: c.Tokens,
+		Tokens: at,
 	}
 	proto := protoMessage{&packet}
 	return proto.writeTo(w)
--- gumble/message.go
+++ gumble/message.go
@@ -5,8 +5,8 @@ import (
 )
 
 // Message is data that be encoded and sent to the server. The following
-// public types implement this interface: BanList, Config, TextMessage, and
-// RegisteredUsers.
+// public types implement this interface: BanList, AccessTokens, TextMessage,
+// and RegisteredUsers.
 type Message interface {
 	writeTo(w io.Writer) (int64, error)
 }
