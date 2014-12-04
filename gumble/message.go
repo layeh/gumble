4ab42bcccba52f0a374c2911e0fe392c54b115ea
Tim Cooper <tim.cooper@layeh.com>
2014-12-04 16:00:42 -0400

    add voice target packet support

--- gumble/message.go
+++ gumble/message.go
@@ -5,8 +5,8 @@ import (
 )
 
 // Message is data that be encoded and sent to the server. The following
-// public types implement this interface: AudioBuffer, AccessTokens, BanList,
-// RegisteredUsers, and TextMessage.
+// types implement this interface: AudioBuffer, AccessTokens, BanList,
+// RegisteredUsers, TextMessage, and VoiceTarget.
 type Message interface {
 	writeTo(client *Client, w io.Writer) (int64, error)
 }
---
+++ gumble/voicetarget.go
@@ -0,0 +1,72 @@
+package gumble
+
+import (
+	"io"
+
+	"code.google.com/p/goprotobuf/proto"
+	"github.com/bontibon/gumble/gumble/MumbleProto"
+)
+
+type voiceTargetChannel struct {
+	channel          *Channel
+	links, recursive bool
+}
+
+type VoiceTarget struct {
+	id       int
+	users    []*User
+	channels []*voiceTargetChannel
+}
+
+// Clear removes all users and channels from the voice target.
+func (vt *VoiceTarget) Clear() {
+	vt.users = nil
+	vt.channels = nil
+}
+
+// Id returns the voice target ID.
+func (vt *VoiceTarget) Id() int {
+	return vt.id
+}
+
+// SetId sets the ID of the voice target. This value must be in the range
+// [1, 30].
+func (vt *VoiceTarget) SetId(id int) {
+	vt.id = id
+}
+
+// AddUser adds a user to the voice target.
+func (vt *VoiceTarget) AddUser(user *User) {
+	vt.users = append(vt.users, user)
+}
+
+// AddChannel adds a user to the voice target.
+func (vt *VoiceTarget) AddChannel(channel *Channel, recursive, links bool) {
+	vt.channels = append(vt.channels, &voiceTargetChannel{
+		channel:   channel,
+		links:     links,
+		recursive: recursive,
+	})
+}
+
+func (vt *VoiceTarget) writeTo(client *Client, w io.Writer) (int64, error) {
+	packet := MumbleProto.VoiceTarget{
+		Id:      proto.Uint32(uint32(vt.id)),
+		Targets: make([]*MumbleProto.VoiceTarget_Target, 0, len(vt.users)+len(vt.channels)),
+	}
+	for _, user := range vt.users {
+		packet.Targets = append(packet.Targets, &MumbleProto.VoiceTarget_Target{
+			Session: []uint32{user.session},
+		})
+	}
+	for _, vtChannel := range vt.channels {
+		packet.Targets = append(packet.Targets, &MumbleProto.VoiceTarget_Target{
+			ChannelId: &vtChannel.channel.id,
+			Links:     &vtChannel.links,
+			Children:  &vtChannel.recursive,
+		})
+	}
+
+	proto := protoMessage{&packet}
+	return proto.writeTo(client, w)
+}
