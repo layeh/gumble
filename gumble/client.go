529e33f6938e6423313a6da95b9eade47ac602f4
Eric R. Schulz <ersmail@gmail.com>
2014-12-07 19:55:20 -0500

    Set opus bitrate within default max bandwidth

--- gumble/client.go
+++ gumble/client.go
@@ -87,6 +87,8 @@ func (c *Client) Connect() error {
 		return err
 	}
 	encoder.SetVbr(false)
+	/* TODO: set this to the server's max bitrate */
+	encoder.SetBitrate(40000)
 	c.audioSequence = 0
 	c.audioTarget = nil
 
