ed8844c0ce10343f9d569562a1a3a05e18b7bfa6
Tim Cooper <tim.cooper@layeh.com>
2014-12-21 18:54:35 -0400

    add --certificate, --key flags to gumbleutil.Main

--- gumbleutil/main.go
+++ gumbleutil/main.go
@@ -1,6 +1,7 @@
 package gumbleutil
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"os"
@@ -9,7 +10,8 @@ import (
 )
 
 // Main aids in the creation of a basic command line gumble bot. It accepts the
-// following flag arguments: --server, --username, --password, and --insecure.
+// following flag arguments: --server, --username, --password, --insecure,
+// --certificate, and --key.
 //
 // If init is non-nil, it is called before attempting to connect to the server.
 func Main(init func(config *gumble.Config, client *gumble.Client), listener gumble.EventListener) {
@@ -17,6 +19,8 @@ func Main(init func(config *gumble.Config, client *gumble.Client), listener gumb
 	username := flag.String("username", "gumble-bot", "client username")
 	password := flag.String("password", "", "client password")
 	insecure := flag.Bool("insecure", false, "skip server certificate verification")
+	certificateFile := flag.String("certificate", "", "user certificate file (PEM)")
+	keyFile := flag.String("key", "", "user certificate key file (PEM)")
 
 	if !flag.Parsed() {
 		flag.Parse()
@@ -33,6 +37,17 @@ func Main(init func(config *gumble.Config, client *gumble.Client), listener gumb
 	client := gumble.NewClient(&config)
 	if *insecure {
 		config.TLSConfig.InsecureSkipVerify = true
+	}
+	if *certificateFile != "" {
+		if *keyFile == "" {
+			keyFile = certificateFile
+		}
+		if certificate, err := tls.LoadX509KeyPair(*certificateFile, *keyFile); err != nil {
+			fmt.Printf("%s: %s\n", os.Args[0], err)
+			os.Exit(1)
+		} else {
+			config.TLSConfig.Certificates = append(config.TLSConfig.Certificates, certificate)
+		}
 	}
 	client.Attach(listener)
 	client.Attach(Listener{
