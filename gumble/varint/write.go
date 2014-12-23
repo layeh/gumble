63e1c73c298d3efa941e40dbf3b254559b7ba53d
Tim Cooper <tim.cooper@layeh.com>
2014-12-23 09:27:33 -0400

    varint: increase range

--- gumble/varint/write.go
+++ gumble/varint/write.go
@@ -1,7 +1,9 @@
 package varint
 
 import (
+	"encoding/binary"
 	"io"
+	"math"
 )
 
 // WriteTo writes the given value to the given io.Writer as a varint encoded
@@ -11,7 +13,7 @@ import (
 // and nil.
 func WriteTo(w io.Writer, value int64) (int64, error) {
 	var length int
-	var buff [4]byte
+	var buff [5]byte
 	if value < 0 {
 		return 0, ErrOutOfRange
 	}
@@ -23,16 +25,20 @@ func WriteTo(w io.Writer, value int64) (int64, error) {
 		buff[1] = byte(value & 0xFF)
 		length = 2
 	} else if value <= 0x1FFFFF {
-		buff[0] = byte((value >> 16) & 0x1F | 0xC0)
+		buff[0] = byte((value>>16)&0x1F | 0xC0)
 		buff[1] = byte((value >> 8) & 0xFF)
 		buff[2] = byte(value & 0xFF)
 		length = 3
 	} else if value <= 0xFFFFFFF {
-		buff[0] = byte((value >> 24) & 0xF | 0xE0)
+		buff[0] = byte((value>>24)&0xF | 0xE0)
 		buff[1] = byte((value >> 16) & 0xFF)
 		buff[2] = byte((value >> 8) & 0xFF)
 		buff[3] = byte(value & 0xFF)
 		length = 4
+	} else if value <= math.MaxInt32 {
+		buff[0] = 0xF0
+		binary.BigEndian.PutUint32(buff[1:], uint32(value))
+		length = 5
 	}
 	if length > 0 {
 		if n, err := w.Write(buff[:length]); err != nil {
