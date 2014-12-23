bebb138f20a66b1f888432153af636166a3dbefe
Tim Cooper <tim.cooper@layeh.com>
2014-12-23 09:15:06 -0400

    format: reduced indentation

--- gumble/varint/read.go
+++ gumble/varint/read.go
@@ -10,30 +10,30 @@ import (
 // consumed, and nil.
 func ReadFrom(r io.Reader) (int64, int64, error) {
 	var buffer [3]byte
-	if b, err := ReadByte(r); err != nil {
+	b, err := ReadByte(r)
+	if err != nil {
 		return 0, 0, err
-	} else {
-		if (b & 0x80) == 0 {
-			return int64(b), 1, nil
+	}
+	if (b & 0x80) == 0 {
+		return int64(b), 1, nil
+	}
+	if (b & 0xC0) == 0x80 {
+		if n, err := io.ReadFull(r, buffer[0:1]); err != nil {
+			return 0, int64(n + 1), err
 		}
-		if (b & 0xC0) == 0x80 {
-			if n, err := io.ReadFull(r, buffer[0:1]); err != nil {
-				return 0, int64(n + 1), err
-			}
-			return int64(b&0x3F)<<8 | int64(buffer[0]), 2, nil
+		return int64(b&0x3F)<<8 | int64(buffer[0]), 2, nil
+	}
+	if (b & 0xE0) == 0xC0 {
+		if n, err := io.ReadFull(r, buffer[0:2]); err != nil {
+			return 0, int64(n + 1), err
 		}
-		if (b & 0xE0) == 0xC0 {
-			if n, err := io.ReadFull(r, buffer[0:2]); err != nil {
-				return 0, int64(n + 1), err
-			}
-			return int64(b&0x1F)<<8 | int64(buffer[0])<<8 | int64(buffer[1]), 3, nil
+		return int64(b&0x1F)<<8 | int64(buffer[0])<<8 | int64(buffer[1]), 3, nil
+	}
+	if (b & 0xF0) == 0xE0 {
+		if n, err := io.ReadFull(r, buffer[0:3]); err != nil {
+			return 0, int64(n + 1), err
 		}
-		if (b & 0xF0) == 0xE0 {
-			if n, err := io.ReadFull(r, buffer[0:3]); err != nil {
-				return 0, int64(n + 1), err
-			}
-			return int64(b&0xF)<<8 | int64(buffer[0])<<8 | int64(buffer[1])<<8 | int64(buffer[2]), 4, nil
-		}
+		return int64(b&0xF)<<8 | int64(buffer[0])<<8 | int64(buffer[1])<<8 | int64(buffer[2]), 4, nil
 	}
 	return 0, 1, ErrOutOfRange
 }
