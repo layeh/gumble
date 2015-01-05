8215d611d67473776438d091278bf21f53df183b
Tim Cooper <tim.cooper@layeh.com>
2015-01-04 20:08:42 -0400

    gumbleutil: add AutoBitrate

---
+++ gumbleutil/bitrate.go
@@ -0,0 +1,22 @@
+package gumbleutil
+
+import (
+	"github.com/layeh/gumble/gumble"
+)
+
+var autoBitrate = &Listener{
+	Connect: func(e *gumble.ConnectEvent) {
+		if e.MaximumBitrate > 0 {
+			dataBytes := e.Client.Config().AudioDataBytes
+			if dataBytes <= 0 {
+				dataBytes = gumble.AudioDefaultDataBytes
+			}
+			bitrate := e.MaximumBitrate - (20 + 8 + 4 + ((1 + 5 + 2 + dataBytes) / 100) * 10) * 8 * 100
+			e.Client.AudioEncoder().SetBitrate(bitrate)
+		}
+	},
+}
+
+// AutoBitrate is a gumble.EventListener that automatically sets the client's
+// maximum outgoing audio bitrate to suitable maximum.
+var AutoBitrate gumble.EventListener = autoBitrate
