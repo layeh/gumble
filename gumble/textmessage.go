256ca6f190780885a62e83b29fa3b267312933dd
Tim Cooper <tim.cooper@layeh.com>
2015-02-07 15:45:31 -0400

    add TextMessage.PlainText()

--- gumble/textmessage.go
+++ gumble/textmessage.go
@@ -1,7 +1,10 @@
 package gumble
 
 import (
+	"bytes"
+	"encoding/xml"
 	"io"
+	"strings"
 
 	"github.com/layeh/gumble/gumble/MumbleProto"
 )
@@ -25,25 +28,61 @@ type TextMessage struct {
 	Message string
 }
 
-func (pm *TextMessage) writeTo(client *Client, w io.Writer) (int64, error) {
-	packet := MumbleProto.TextMessage{
-		Message: &pm.Message,
+// PlainText returns the Message string without HTML tags or entities.
+func (tm *TextMessage) PlainText() string {
+	d := xml.NewDecoder(strings.NewReader(tm.Message))
+	d.Strict = false
+	d.AutoClose = xml.HTMLAutoClose
+	d.Entity = xml.HTMLEntity
+
+	var b bytes.Buffer
+	newline := false
+	for {
+		t, _ := d.Token()
+		if t == nil {
+			break
+		}
+		switch node := t.(type) {
+		case xml.CharData:
+			if len(node) > 0 {
+				b.Write(node)
+				newline = false
+			}
+		case xml.StartElement:
+			switch node.Name.Local {
+			case "address", "article", "aside", "audio", "blockquote", "canvas", "dd", "div", "dl", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "header", "hgroup", "hr", "noscript", "ol", "output", "p", "pre", "section", "table", "tfoot", "ul", "video":
+				if !newline {
+					b.WriteByte('\n')
+					newline = true
+				}
+			case "br":
+				b.WriteByte('\n')
+				newline = true
+			}
+		}
 	}
-	if pm.Users != nil {
-		packet.Session = make([]uint32, len(pm.Users))
-		for i, user := range pm.Users {
+	return b.String()
+}
+
+func (tm *TextMessage) writeTo(client *Client, w io.Writer) (int64, error) {
+	packet := MumbleProto.TextMessage{
+		Message: &tm.Message,
+	}
+	if tm.Users != nil {
+		packet.Session = make([]uint32, len(tm.Users))
+		for i, user := range tm.Users {
 			packet.Session[i] = user.session
 		}
 	}
-	if pm.Channels != nil {
-		packet.ChannelId = make([]uint32, len(pm.Channels))
-		for i, channel := range pm.Channels {
+	if tm.Channels != nil {
+		packet.ChannelId = make([]uint32, len(tm.Channels))
+		for i, channel := range tm.Channels {
 			packet.ChannelId[i] = channel.id
 		}
 	}
-	if pm.Trees != nil {
-		packet.TreeId = make([]uint32, len(pm.Trees))
-		for i, channel := range pm.Trees {
+	if tm.Trees != nil {
+		packet.TreeId = make([]uint32, len(tm.Trees))
+		for i, channel := range tm.Trees {
 			packet.TreeId[i] = channel.id
 		}
 	}
