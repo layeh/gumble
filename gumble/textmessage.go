313707b6f7c2699602dbd07e244ad2768d25b581
Tim Cooper <tim.cooper@layeh.com>
2015-02-07 16:03:07 -0400

    move TextMessage.PlainText to gumbleutil

--- gumble/textmessage.go
+++ gumble/textmessage.go
@@ -1,10 +1,7 @@
 package gumble
 
 import (
-	"bytes"
-	"encoding/xml"
 	"io"
-	"strings"
 
 	"github.com/layeh/gumble/gumble/MumbleProto"
 )
@@ -26,42 +23,6 @@ type TextMessage struct {
 
 	// Chat message.
 	Message string
-}
-
-// PlainText returns the Message string without HTML tags or entities.
-func (tm *TextMessage) PlainText() string {
-	d := xml.NewDecoder(strings.NewReader(tm.Message))
-	d.Strict = false
-	d.AutoClose = xml.HTMLAutoClose
-	d.Entity = xml.HTMLEntity
-
-	var b bytes.Buffer
-	newline := false
-	for {
-		t, _ := d.Token()
-		if t == nil {
-			break
-		}
-		switch node := t.(type) {
-		case xml.CharData:
-			if len(node) > 0 {
-				b.Write(node)
-				newline = false
-			}
-		case xml.StartElement:
-			switch node.Name.Local {
-			case "address", "article", "aside", "audio", "blockquote", "canvas", "dd", "div", "dl", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "header", "hgroup", "hr", "noscript", "ol", "output", "p", "pre", "section", "table", "tfoot", "ul", "video":
-				if !newline {
-					b.WriteByte('\n')
-					newline = true
-				}
-			case "br":
-				b.WriteByte('\n')
-				newline = true
-			}
-		}
-	}
-	return b.String()
 }
 
 func (tm *TextMessage) writeTo(client *Client, w io.Writer) (int64, error) {
