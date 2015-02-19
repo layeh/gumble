29411f8e62add7e8f0856b35dc925cf879be3aab
Tim Cooper <tim.cooper@layeh.com>
2015-02-19 15:13:18 -0400

    improve gumbleutil.CertificateLockFile
    the function now verifies that every certificate in the chain is the same

--- gumbleutil/certificatelock.go
+++ gumbleutil/certificatelock.go
@@ -4,64 +4,72 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"io/ioutil"
 	"os"
 
 	"github.com/layeh/gumble/gumble"
 )
 
-// CertificateLockFile adds a new certificate lock on the given Client and
-// Config that ensures that a server's certificate is signed by the same CA
-// from connection-to-connection. This is helpful when connecting to servers
-// with self-signed certificates.
+// CertificateLockFile adds a new certificate lock on the given Client that
+// ensures that a server's certificate chain is the same from
+// connection-to-connection. This is helpful when connecting to servers with
+// self-signed certificates.
 //
 // If filename does not exist, the server's certificate chain will be written
-// to that file. If it does exist, certificates will be read from that file and
-// added to RootCAs in config's TLSConfig.
+// to that file. If it does exist, certificates will be read from the file and
+// checked against the server's certificate chain upon connection.
 //
 // Example:
 //
-//  if firstConnectionToServer {
-//      // Allow self-signed certificates to be accepted on the initial
-//      // connection.
+//  if allowSelfSignedCertificates {
 //      config.TLSConfig.InsecureSkipVerify = true
 //  }
-//  gumbleutil.CertificateLockFile(client, &config, filename)
+//  gumbleutil.CertificateLockFile(client, filename)
 //
 //  if err := client.Connect(); err != nil {
 //      panic(err)
 //  }
-func CertificateLockFile(client *gumble.Client, config *gumble.Config, filename string) (gumble.Detacher, error) {
-	if file, err := os.Open(filename); err == nil {
-		defer file.Close()
-		if config.TLSConfig.RootCAs == nil {
-			config.TLSConfig.RootCAs = x509.NewCertPool()
-		}
-		if data, err := ioutil.ReadAll(file); err == nil {
-			config.TLSConfig.RootCAs.AppendCertsFromPEM(data)
-		}
-		return nil, nil
-	}
-
-	return client.Attach(Listener{
-		Connect: func(e *gumble.ConnectEvent) {
-			tlsClient, ok := e.Client.Conn.Conn.(*tls.Conn)
-			if !ok {
-				return
-			}
-			serverCerts := tlsClient.ConnectionState().PeerCertificates
-			file, err := os.Create(filename)
+func CertificateLockFile(client *gumble.Client, filename string) {
+	client.Config.TLSVerify = func(state *tls.ConnectionState) error {
+		if file, err := os.Open(filename); err == nil {
+			defer file.Close()
+			data, err := ioutil.ReadAll(file)
 			if err != nil {
-				return
+				return err
 			}
-			block := pem.Block{
-				Type: "CERTIFICATE",
+			i := 0
+			for block, data := pem.Decode(data); block != nil; block, data = pem.Decode(data) {
+				cert, err := x509.ParseCertificate(block.Bytes)
+				if err != nil {
+					return err
+				}
+				if i >= len(state.PeerCertificates) {
+					return errors.New("gumbleutil: invalid certificate chain length")
+				}
+				if !cert.Equal(state.PeerCertificates[i]) {
+					return errors.New("gumbleutil: certificate verification failure")
+				}
+				i++
 			}
-			for _, cert := range serverCerts {
-				block.Bytes = cert.Raw
-				pem.Encode(file, &block)
+			if i != len(state.PeerCertificates) {
+				return errors.New("gumbleutil: invalid certificate chain length")
 			}
-			file.Close()
-		},
-	}), nil
+			return nil
+		}
+
+		file, err := os.Create(filename)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		block := pem.Block{
+			Type: "CERTIFICATE",
+		}
+		for _, cert := range state.PeerCertificates {
+			block.Bytes = cert.Raw
+			pem.Encode(file, &block)
+		}
+		return nil
+	}
 }
