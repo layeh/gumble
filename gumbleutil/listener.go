abf58b0ea8b2661897f81cf69c2a6a3e37152d74
Tim Cooper <tim.cooper@layeh.com>
2015-04-30 10:43:14 -0300

    add gumbleutil.ListenerFunc

--- gumbleutil/listener.go
+++ gumbleutil/listener.go
@@ -4,10 +4,8 @@ import (
 	"github.com/layeh/gumble/gumble"
 )
 
-// Listener is a struct that implements the gumble.EventListener interface,
-// allowing it to be attached to a Client. This is useful if you would like to
-// have a few specific listeners, rather than the whole gumble.EventListener
-// interface.
+// Listener is a struct that implements the gumble.EventListener interface. The
+// corresponding event function in the struct is called if it is non-nil.
 type Listener struct {
 	Connect             func(e *gumble.ConnectEvent)
 	Disconnect          func(e *gumble.DisconnectEvent)
@@ -21,71 +19,63 @@ type Listener struct {
 	ContextActionChange func(e *gumble.ContextActionChangeEvent)
 }
 
-// OnConnect implements gumble.EventListener.OnConnect. Calls l.Connect if it is
-// non-nil.
+// OnConnect implements gumble.EventListener.OnConnect.
 func (l Listener) OnConnect(e *gumble.ConnectEvent) {
 	if l.Connect != nil {
 		l.Connect(e)
 	}
 }
 
-// OnDisconnect implements gumble.EventListener.OnDisconnect. Calls
-// l.Disconnect if it is non-nil.
+// OnDisconnect implements gumble.EventListener.OnDisconnect.
 func (l Listener) OnDisconnect(e *gumble.DisconnectEvent) {
 	if l.Disconnect != nil {
 		l.Disconnect(e)
 	}
 }
 
-// OnTextMessage implements gumble.EventListener.OnTextMessage. Calls
-// l.TextMessage if it is non-nil.
+// OnTextMessage implements gumble.EventListener.OnTextMessage.
 func (l Listener) OnTextMessage(e *gumble.TextMessageEvent) {
 	if l.TextMessage != nil {
 		l.TextMessage(e)
 	}
 }
 
-// OnUserChange implements gumble.EventListener.OnUserChange. Calls
-// l.UserChange if it is non-nil.
+// OnUserChange implements gumble.EventListener.OnUserChange.
 func (l Listener) OnUserChange(e *gumble.UserChangeEvent) {
 	if l.UserChange != nil {
 		l.UserChange(e)
 	}
 }
 
-// OnChannelChange implements gumble.EventListener.OnChannelChange. Calls
-// l.ChannelChange if it is non-nil.
+// OnChannelChange implements gumble.EventListener.OnChannelChange.
 func (l Listener) OnChannelChange(e *gumble.ChannelChangeEvent) {
 	if l.ChannelChange != nil {
 		l.ChannelChange(e)
 	}
 }
 
-// OnPermissionDenied implements gumble.EventListener.OnPermissionDenied. Calls
-// l.PermissionDenied if it is non-nil.
+// OnPermissionDenied implements gumble.EventListener.OnPermissionDenied.
 func (l Listener) OnPermissionDenied(e *gumble.PermissionDeniedEvent) {
 	if l.PermissionDenied != nil {
 		l.PermissionDenied(e)
 	}
 }
 
-// OnUserList implements gumble.EventListener.OnUserList. Calls l.UserList if
-// it is non-nil.
+// OnUserList implements gumble.EventListener.OnUserList.
 func (l Listener) OnUserList(e *gumble.UserListEvent) {
 	if l.UserList != nil {
 		l.UserList(e)
 	}
 }
 
-// OnACL implements gumble.EventListener.OnACL. Calls l.ACL if it is non-nil.
+// OnACL implements gumble.EventListener.OnACL.
 func (l Listener) OnACL(e *gumble.ACLEvent) {
 	if l.ACL != nil {
 		l.ACL(e)
 	}
 }
 
-// OnBanList implements gumble.EventListener.OnBanList. Calls l.BanList if it
-// is non-nil.
+// OnBanList implements gumble.EventListener.OnBanList.
 func (l Listener) OnBanList(e *gumble.BanListEvent) {
 	if l.BanList != nil {
 		l.BanList(e)
@@ -93,7 +83,6 @@ func (l Listener) OnBanList(e *gumble.BanListEvent) {
 }
 
 // OnContextActionChange implements gumble.EventListener.OnContextActionChange.
-// Calls l.ContextActionChange if it is non-nil.
 func (l Listener) OnContextActionChange(e *gumble.ContextActionChangeEvent) {
 	if l.ContextActionChange != nil {
 		l.ContextActionChange(e)
