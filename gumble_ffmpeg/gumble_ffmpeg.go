0b65794935bac2c4c9e8bb4d56007fd2c7b16e26
Tim Cooper <tim.cooper@layeh.com>
2015-05-09 07:37:11 -0300

    gumble_ffmpeg: fixes
    Stream.Offset now allows for fractions of a second
    remove useless slice unpacking

--- gumble_ffmpeg/gumble_ffmpeg.go
+++ gumble_ffmpeg/gumble_ffmpeg.go
@@ -56,10 +56,10 @@ func (s *Stream) Play() error {
 		return errors.New("nil source")
 	}
 	args := s.Source.arguments()
-	if secs := int(s.Offset.Seconds()); secs > 0 {
-		args = append([]string{"-ss", strconv.Itoa(secs)}, args...)
+	if s.Offset > 0 {
+		args = append([]string{"-ss", strconv.FormatFloat(s.Offset.Seconds(), 'f', -1, 64)}, args...)
 	}
-	args = append(args, []string{"-ac", "1", "-ar", strconv.Itoa(gumble.AudioSampleRate), "-f", "s16le", "-"}...)
+	args = append(args, "-ac", "1", "-ar", strconv.Itoa(gumble.AudioSampleRate), "-f", "s16le", "-")
 	cmd := exec.Command(s.Command, args...)
 	if pipe, err := cmd.StdoutPipe(); err != nil {
 		return err
