d847df0f20bc0329f7d0bf5b18904d1bb588b9e8
Tim Cooper <tim.cooper@layeh.com>
2020-03-22 15:42:57 -0500

    move MumbleProto generation to Go

--- gumble/MumbleProto/generate.go
+++ gumble/MumbleProto/generate.go
@@ -1,5 +1,2 @@
-//go:generate wget -O Mumble.proto https://raw.githubusercontent.com/mumble-voip/mumble/master/src/Mumble.proto
-//go:generate protoc --go_out=. Mumble.proto
-//go:generate rm -f Mumble.proto
-//go:generate sed -i "s/^\\(package MumbleProto\\)$/\\1 \\/\\/ import \"layeh.com\\/gumble\\/gumble\\/MumbleProto\"/" Mumble.pb.go
+//go:generate go run generate_main.go
 package MumbleProto
